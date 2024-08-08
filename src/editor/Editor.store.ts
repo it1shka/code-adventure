80ea137ff947d0b3f1f92d0739caccedc449835f
it1shka <2tishbel@gmail.com>
2024-08-08 16:01:54 +0200

    Added localStorage logic to persist the code

--- src/editor/Editor.store.ts
+++ src/editor/Editor.store.ts
@@ -1,5 +1,8 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+
+const STORAGE_KEY = 'editor-code'
+const AUTOSAVE_TIME = 500
 
 const FontSizeSettings = Object.freeze({
   min: 12,
@@ -7,11 +10,26 @@ const FontSizeSettings = Object.freeze({
   max: 30,
 })
 
+const loadFromStorage = () => {
+  const code = window
+    .localStorage
+    .getItem(STORAGE_KEY)
+  return code ?? ''
+}
+
 export const useEditorStore = defineStore('editor', () => {
-  const sourceCode = ref('')
+  const sourceCode = ref(loadFromStorage())
   const setSourceCode = (code: string) => {
     sourceCode.value = code
   }
+
+  const timeout = ref<ReturnType<typeof setTimeout>>()
+  watch(sourceCode, () => {
+    clearTimeout(timeout.value)
+    timeout.value = setTimeout(() => {
+      window.localStorage.setItem(STORAGE_KEY, sourceCode.value)
+    }, AUTOSAVE_TIME)
+  })
 
   const vimMode = ref(false)
   const toggleVimMode = () => {
